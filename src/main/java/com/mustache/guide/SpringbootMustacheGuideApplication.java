acf641a9401ceff7ecaf2207f48e092e1f9149bc
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-17 19:14:28 +0900

    2022 11 17 - first commit

---
+++ src/main/java/com/mustache/guide/SpringbootMustacheGuideApplication.java
@@ -0,0 +1,13 @@
+package com.mustache.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMustacheGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMustacheGuideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mustache/guide/SpringbootMustacheGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache.guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMustacheGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
